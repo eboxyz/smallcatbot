8976470916618c017911bb40ac3f3289e819e1a5
edwardyu <edward.yu@reflexionhealth.com>
2018-10-09 12:50:04 -0700

    add cron jobs for raid times and banquets

--- app.js
+++ app.js
@@ -1,74 +1,85 @@
-// const request = require('request');
-const fetch = require('node-fetch');
-// const {postData} = require('./helpers/fetch_mods') 
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require('./config');
+const cron = require('cron').CronJob;
 
-// const url = 'https://discordapp.com/channels/177562553461899264/177562553461899264/messages?limit=50'
-// const url = 'https://discordapp.com/channels/177562553461899264/177562553461899264'
-const url = `https://discordapp.com/api`
-const headers = {
-        'Authorization': 'Bot ' + config.token,
-        'Accept': 'application/json',
-        'User-Agent': 'DiscordBot ()'
-}
-
-client.on("ready", () => {
-  console.log("I am ready!");
-});
-
-const commands = "`!raid`, `!server`, `!happy`, `!cuddle`, `!bitchwhat`, `!wink`, `!nudes`, `hi smallcat`, `help me smallcat`"
+const commands = "`!raid`, `!wrongchat`, `!banquet`, `!server`, `!happy`, `!cuddle`, `!bitchwhat`, `!wink`, `!nudes`, `hi smallcat`, `help me smallcat`"
 
 client.on("message", (message) => {
+    console.log(message.author.username + ": " + message.content)
 
-console.log(message.author.username + ": " + message.content)
-  if (message.content.startsWith(`${config.prefix}commands`)) {
-      message.channel.send(`My current commands are: ${commands}`)
-  }
-  if (message.content.startsWith(`${config.prefix}raid`)) {
-      message.channel.send("@here: https://imgur.com/OqRXrBV")
-  }
-  else if (message.content.startsWith(`${config.prefix}server`)) {
-      message.channel.send(`This server is: ${message.guild.name}`);
-  }
-  else if (message.content.startsWith("hi smallcat")) {
-    const smallyell = client.emojis.find("name", "smallyell")
-    message.channel.send(`${smallyell}`)
-  }
-  else if (message.content.startsWith("help me smallcat")) {
-    const smallyell = client.emojis.find("name", "smallyell")
-    message.channel.send(`${smallyell} http://bfy.tw/3nV`)
-  }
-  else if (message.content.startsWith(`${config.prefix}happy`)) {
-    message.channel.send("https://imgur.com/VKbBiK1")
-  }
-  else if (message.content.startsWith(`${config.prefix}cuddle`)) {
-    message.channel.send("https://imgur.com/VkZGqXb")
-  }
-  else if (message.content.startsWith(`${config.prefix}bitchwhat`)) {
-    message.channel.send("https://imgur.com/jiWgckm")
-  }
-  else if (message.content.startsWith(`${config.prefix}nudes`)) {
-    message.channel.send("https://i.imgur.com/fjj9HHl.jpg")
-  }
-  else if (message.content.startsWith(`${config.prefix}wink`)) {
-      message.channel.send("https://imgur.com/Of6iJPr")
-  }
-  else if (message.content.startsWith(`${config.prefix}messages`)) {
+    if (message.content.startsWith(`${config.prefix}commands`)) {
+        message.channel.send(`My current commands are: ${commands}`)
+    }
+    else if (message.content.startsWith(`${config.prefix}please`)) {
+        message.channel.send(`https://imgur.com/Y4hKCnB`)
+    }
+    else if (message.content.startsWith(`${config.prefix}wrongchat`)) {
+        message.channel.send(`https://imgur.com/CLD8XXW`)
+    }
+    else if (message.content.startsWith(`${config.prefix}banquet`)) {
+        message.channel.send(`https://imgur.com/zeuN3ZR`)
+    }
+    else if (message.content.startsWith(`${config.prefix}raid`)) {
+        message.channel.send("@here: https://imgur.com/Ep26FN8")
+    }
+    else if (message.content.startsWith(`${config.prefix}server`)) {
+        message.channel.send(`This server is: ${message.guild.name}`);
+    }
+    else if (message.content.startsWith("hi smallcat")) {
+        const smallyell = client.emojis.find("name", "smallyell")
+        message.channel.send(`${smallyell}`)
+    }
+    else if (message.content.startsWith("help me smallcat")) {
+        const smallyell = client.emojis.find("name", "smallyell")
+        message.channel.send(`${smallyell} http://bfy.tw/3nV`)
+    }
+    else if (message.content.startsWith(`${config.prefix}happy`)) {
+        message.channel.send("https://imgur.com/VKbBiK1")
+    }
+    else if (message.content.startsWith(`${config.prefix}cuddle`)) {
+        message.channel.send("https://imgur.com/VkZGqXb")
+    }
+    else if (message.content.startsWith(`${config.prefix}bitchwhat`)) {
+        message.channel.send("https://imgur.com/jiWgckm")
+    }
+    else if (message.content.startsWith(`${config.prefix}nudes`)) {
+        message.channel.send("https://i.imgur.com/fjj9HHl.jpg")
+    }
+    else if (message.content.startsWith(`${config.prefix}wink`)) {
+        message.channel.send("https://imgur.com/Of6iJPr")
+    }
+    else if (message.content.startsWith(`${config.prefix}messages`)) {
     //message.channel will always refer to the current channel that this is invoked in
-    message.channel.fetchMessages()
-        .then( messages => messages.map( message => message.delete() ) )
-        .catch(console.error);
-  }
-//   else if (message.content.startsWith(`${config.prefix}me`)) {
-//     message.channel.send(`You are: ${message.author.username}`)
-//   }
-//   else if (message.content.startsWith("y")){
-//     fetch(url)
-//         .then(response => response.text())
-//         .then(data => {console.log(JSON.stringify(data)); })
-//   };
+        message.channel.fetchMessages()
+            .then( messages => messages.map( message => message.delete() ) )
+            .catch(console.error);
+    }
 });
 
-client.login(config.token);
+client.login(config.token).then(() => { console.log('meow!' )});
+
+const raidjob1 = new cron('0 0 13 * * *', function(){
+    const guild = client.guilds.get('472829226094166034')
+    const channel = guild.channels.get('472829227159257099')
+    channel.send("!raid")
+})
+raidjob1.start();
+
+const raidjob2 = new cron('0 0 21 * * *', function(){
+    const guild = client.guilds.get('472829226094166034')
+    const channel = guild.channels.get('475054543022063637')
+    channel.send("!raid")
+})
+raidjob2.start();
+
+const banquetjob = new cron('0 0 19 * * *', function(){
+    const guild = client.guilds.get('472829226094166034')
+    const channel = guild.channels.get('475054543022063637')
+    channel.send("!raid")
+})
+banquetjob.start();
+
+console.log('raidjob1 is running?', raidjob1.running)
+console.log('raidjob2 is running?', raidjob2.running)
+console.log('banquetjob is running?', banquetjob.running)
